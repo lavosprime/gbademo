3244912f566fbfc74986893e145f69112b559b44
Cooper Johnson <lavosprime@gmail.com>
2014-02-10 19:59:06 -0800

    Update Makefile and begin demos.h

---
+++ include/demos.h
@@ -0,0 +1,27 @@
+/*
+ * Header file for linking the various demos into main
+ */
+
+#ifndef _demos_h
+#define _demos_h
+
+// 8 different demos are available
+#define NUM_DEMOS (8)
+
+// type "demo" is a pointer to a function that takes no args and returns void
+typedef void (*demo)();
+
+// each demo is activated by calling its "main" method
+void demo1_main();
+void demo2_main();
+void demo3_main();
+void demo4_main();
+void demo5_main();
+void demo6_main();
+void demo7_main();
+void demo8_main();
+
+// a placeholder for unimplemented demos
+static inline void no_demo() {  }
+
+#endif
