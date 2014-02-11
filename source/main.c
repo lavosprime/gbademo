a5be71772b988883202afb324a997cec169af00f
Cooper Johnson <lavosprime@gmail.com>
2014-02-10 20:43:16 -0800

    Add basic function-pointer logic in main

--- source/main.c
+++ source/main.c
@@ -1,7 +1,51 @@
 #include <gba.h>
+#include <gba_types.h>
+#include "demos.h"
+
+static inline void libgba_init(void) {
+	//TODO
+}
+
+static inline void main_init(void) {
+	//TODO
+}
 
 int main(void)
 {
-	while (1);
-	return 0;
+	libgba_init();
+
+	int selection = 0;
+	bool needs_init = true;
+	const demo demos[NUM_DEMOS] = {	demo1_main, demo2_main,
+									demo3_main, demo4_main,
+									demo5_main, demo6_main,
+									demo7_main, demo8_main};
+
+	for(;;) {
+		if (needs_init) {
+			main_init();
+			needs_init = false;
+		}
+
+		//TODO button logic
+
+		if (0) {
+			demos[selection]();
+			needs_init = true;
+		}
+
+		//TODO wait for vblank
+	}
+
+	return 0;	// never actually get here; undefined behavior on GBA
 }
+
+// safety placeholders for unimplemented demos
+void demo1_main() { no_demo(); }
+void demo2_main() { no_demo(); }
+void demo3_main() { no_demo(); }
+void demo4_main() { no_demo(); }
+void demo5_main() { no_demo(); }
+void demo6_main() { no_demo(); }
+void demo7_main() { no_demo(); }
+void demo8_main() { no_demo(); }
