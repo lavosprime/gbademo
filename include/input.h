ae9089b8fbc8c197e9360763df04fb24d659ebec
Cooper Johnson <lavosprime@gmail.com>
2014-03-06 19:39:15 -0800

    Implement discrete input refreshing and input tribools

--- include/input.h
+++ include/input.h
@@ -1,8 +1,70 @@
+//==============================================================================
 /*
- * Header file for various button input functions
- */
+    Header file for various button input functions
+
+    Copyright 2014 by Cooper Johnson.
+
+    This program is free software provided under the terms of the MIT License.
+    It uses libgba, which is provided under the terms of the GNU LGPL v2 by the
+    devkitpro project (http://sourceforge.net/projects/devkitpro).
+*/
+//==============================================================================
 
 #ifndef _input_h
 #define _input_h
+
+#include <gba_types.h>
+
+/*------------------------------------------------------------------------------
+ * Functions for frame-locking of input logic and acquiring input state
+ *----------------------------------------------------------------------------*/
+
+/*
+ * Updates internal tracking of current and previous key states.
+ */
+inline void refresh_keys();
+
+/*
+ * Returns the key state as of the most recent call to refresh_keys().
+ */
+inline u16 get_curr_keys();
+
+/*
+ * Returns the key state from before the most recent call to refresh_keys().
+ */
+inline u16 get_prev_keys();
+
+/*------------------------------------------------------------------------------
+ * "Tribool" values of -1 for left/up, 1 for right/down, or 0 for none/both.
+ * Tribools enable simple addition instead of if/else blocks for handling
+ * directional controls.
+ *----------------------------------------------------------------------------*/
+
+/*
+ * Converts the given boolean values representing a "positive" value and a
+ * "negative" value to a corresponding tribool.
+ */
+#define TRIBOOL(POS, NEG) ((!(POS))^1 - (!(NEG))^1)
+
+/*
+ * Returns 1 if the given key state includes down on the d-pad but not up,
+ * returns -1 if the given key state includes up on the d-pad but not down,
+ * or returns 0 if the given key state includes both or neither.
+ */
+inline int tribool_dpad_vert(u16 key_state);
+
+/*
+ * Returns 1 if the given key state includes down on the d-pad but not up,
+ * returns -1 if the given key state includes up on the d-pad but not down,
+ * or returns 0 if the given key state includes both or neither.
+ */
+inline int tribool_dpad_horz(u16 key_state);
+
+/*
+ * Returns 1 if the given key state includes the R shoulder button but not L,
+ * returns -1 if the given key state includes the L shoulder button but not R,
+ * or returns 0 if the given key state includes both or neither.
+ */
+inline int tribool_shoulder(u16 key_state);
 
 #endif // _input_h
--- source/input.c
+++ source/input.c
@@ -1,5 +1,48 @@
+//==============================================================================
 /*
- * Implementation of non-inline functions of input.h
- */
+    Implementation of functions defined in input.h
 
+    Copyright 2014 by Cooper Johnson.
+
+    This program is free software provided under the terms of the MIT License.
+    It uses libgba, which is provided under the terms of the GNU LGPL v2 by the
+    devkitpro project (http://sourceforge.net/projects/devkitpro).
+*/
+//==============================================================================
+
+#include <gba_input.h>
 #include "input.h"
+
+static u16 curr_keys;
+static u16 prev_keys;
+
+inline void refresh_keys()
+{
+    prev_keys = curr_keys;
+    curr_keys = ~REG_KEYINPUT;
+}
+
+inline u16 get_curr_keys()
+{
+    return curr_keys;
+}
+
+inline u16 get_prev_keys()
+{
+    return prev_keys;
+}
+
+inline int tribool_dpad_vert(u16 key_state)
+{
+    return TRIBOOL((key_state & KEY_DOWN), (key_state & KEY_UP));
+}
+
+inline int tribool_dpad_horz(u16 key_state)
+{
+    return TRIBOOL((key_state & KEY_RIGHT), (key_state & KEY_LEFT));
+}
+
+inline int tribool_shoulder(u16 key_state)
+{
+    return TRIBOOL((key_state & KEY_R), (key_state & KEY_L));
+}
